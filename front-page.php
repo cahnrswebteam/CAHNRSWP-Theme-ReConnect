3d9c74d1729bd86ca21930a7affc17f0aa673279
Phil Cable <pcable@wsu.edu>
2016-01-06 13:04:32 -0800

    Additions for 2016 issue

--- front-page.php
+++ front-page.php
@@ -1,4 +1,7 @@
 <?php
-$year = date( 'Y' );
+$most_recent_published_post = get_posts( 'posts_per_page=1' );
+foreach( $most_recent_published_post as $post ) {
+	$year = get_the_date( 'Y' );
+}
 get_template_part( 'toc', $year );
 ?>
